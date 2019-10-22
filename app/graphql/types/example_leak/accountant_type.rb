b51278c52641e42ee18d4febf9b1b7806309b49d
Ahmad Ali <i@ahmadali.info>
2019-10-22 19:12:28 +0500

    company_leak_fix

--- app/graphql/types/example_leak/accountant_type.rb
+++ app/graphql/types/example_leak/accountant_type.rb
@@ -3,7 +3,10 @@ module Types
     class AccountantType < BaseObject
       field :id, ID, null: false
       field :name, String, null: false
-      field :transaction_logs, [Types::ExampleLeak::TransactionLogType], null: true
+      field :transaction_logs, [Types::ExampleLeak::TransactionLogType], null: true,
+        resolve: ->(obj, args, ctx) {
+          TransactionLog.where(id: ctx.irep_node.parent.parent.arguments[:id])
+        }
       field :companies, [Types::ExampleLeak::CompanyType], null: true
     end
   end
